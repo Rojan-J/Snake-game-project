6e89caf61e4fe3ddbeb6b3389ff845880a159278
Niloofar Torki <niloofar.torki1381@gmail.com>
2024-11-11 01:22:15 +0330

    graphical changes, start and game over page added

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ class Main:
     
     def __init__(self):
         self.snake=Snake()
-        self.fruit=Fruits()  
+        self.fruit=Fruits()
+        self.score = 0
         
     def update(self):
         self.snake.snake_moving()
@@ -27,6 +28,7 @@ class Main:
 
     
         if snake_head_rect.colliderect(self.fruit.rect):
+            self.score += 5
             self.fruit.randomize() 
             self.snake.add_block()
         #if self.fruit.pos== self.snake.body[0]:
@@ -52,6 +54,7 @@ class Fruits:
         self.x= random.randint(0,18)
         self.y= random.randint(0,18)
         self.pos=Vector2(self.x,self.y)
+        self.rect = pygame.Rect(-1, -1, 1, 1)
         
     def draw_fruit(self):
         apple = pygame.image.load("Project/Snake-game-project/apple1.png").convert_alpha()
@@ -75,16 +78,18 @@ class Snake:
         tongue = pygame.image.load("Project/Snake-game-project/tongue.png").convert_alpha()
         tongue_scaled = pygame.transform.scale(tongue, (40, 40))
 
-
-
+        end_eye = pygame.image.load("Project/Snake-game-project/end_eye.png")
     
         self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
         self.direction = Vector2(1,0)
+        self.last_body = None
         self.new_block=False
         self.eyes = [open_eyes_scaled, close_eyes_scaled]
+        self.eyes_index = 0
+        self.end_eyes = pygame.transform.scale(end_eye, (40, 40))
         self.tongue = tongue_scaled
         self.tongue_index = 0
-        self.eyes_index = 0
+
         
     def draw_snake(self):
         for index, block in enumerate(self.body):
@@ -167,7 +172,63 @@ class Snake:
             else:  # Other blocks without rounded corners
                 pygame.draw.rect(game_screen, "#5a6f19", block_rect) 
             
+    def draw_end_snake(self):
+        for index, block in enumerate(self.body):
+            x=int(block.x*40)
+            y=int(block.y*40)
+            block_rect=pygame.Rect(x,y,40,40)
+            
+            if index == 0:  # Head of the snake               
+                # Determine which corners to round based on direction
+                if self.direction == Vector2(1, 0):  # Moving right
+                    pygame.draw.rect(game_screen, "#5a6f19", block_rect, border_top_right_radius=10, border_bottom_right_radius= 10)  # Top-right corner
+                    end_eyes_scaled = pygame.transform.rotate(self.end_eyes, 0)
+                    end_eyes_rect = end_eyes_scaled.get_rect(center=(x + 20, y))
+                    rotated_tongue = pygame.transform.rotate(self.tongue, 180)
+                    pygame.draw.circle(game_screen, "Black", (x + 35, y+25), 5, 5)
+                    tongue_rect = rotated_tongue.get_rect(midleft = (x + 35, y+ 20))
+
+                    
+                elif self.direction == Vector2(-1, 0):  # Moving left
+                    pygame.draw.rect(game_screen, "#5a6f19", block_rect, border_top_left_radius=10, border_bottom_left_radius= 10)  # Top-left corner
+                    end_eyes_scaled = pygame.transform.rotate(self.end_eyes, 0)
+                    end_eyes_rect = end_eyes_scaled.get_rect(center=(x + 20, y))
+                    pygame.draw.circle(game_screen, "Black", (x +5, y+25), 5, 5)
+                    rotated_tongue= pygame.transform.rotate(self.tongue, 0)
+                    tongue_rect = rotated_tongue.get_rect(midright = (x + 6, y+ 30))
+
+
+                elif self.direction == Vector2(0, -1):  # Moving up
+                    pygame.draw.rect(game_screen, "#5a6f19", block_rect, border_top_left_radius=10, border_top_right_radius= 10)  # Top-left corner
+                    end_eyes_scaled = pygame.transform.rotate(self.end_eyes, -90)
+                    end_eyes_rect = end_eyes_scaled.get_rect(center=(x + 35, y +20))
+                    pygame.draw.circle(game_screen, "Black", (x +15, y+5), 5, 5)
+                    rotated_tongue= pygame.transform.rotate(self.tongue, -90)
+                    tongue_rect = rotated_tongue.get_rect(midbottom = (x + 8, y+ 10))
+
+
+
+                elif self.direction == Vector2(0, 1):  # Moving down
+                    pygame.draw.rect(game_screen, "#5a6f19", block_rect, border_bottom_left_radius=10, border_bottom_right_radius= 10)  # Bottom-left corner
+                    end_eyes_scaled = pygame.transform.rotate(self.end_eyes, -90)
+                    end_eyes_rect = end_eyes_scaled.get_rect(center=(x + 35, y +20))
+                    pygame.draw.circle(game_screen, "Black", (x +15, y+35), 5, 5)
+                    rotated_tongue= pygame.transform.rotate(self.tongue, 90)
+                    tongue_rect = rotated_tongue.get_rect(midtop = (x + 20, y+ 32))
+
+                game_screen.blit(rotated_tongue, tongue_rect)
+                game_screen.blit(end_eyes_scaled, end_eyes_rect)
+
+            else:  # Other blocks without rounded corners
+                pygame.draw.rect(game_screen, "#5a6f19", block_rect) 
+
+
+    def undo(self):
+        self.body = self.last_body
+
+
     def snake_moving(self):
+        self.last_body = self.body[:]
         if self.new_block==True:
             prev_body=self.body[:]
             prev_body.insert(0,prev_body[0]+self.direction)
@@ -184,7 +245,8 @@ class Snake:
     def add_block(self):
         self.new_block=True
 
-game_state = True
+game_state = False
+
 
 def check_game_over(main):
     global game_state
@@ -207,13 +269,49 @@ background = pygame.transform.scale(background, (800, 800))
 
 clock=pygame.time.Clock()
 
+game_font = pygame.font.Font("Project/Snake-game-project/JungleAdventurer.ttf", 150)
+
+
+start_surf = pygame.image.load("Project/Snake-game-project/start_page.jpg").convert_alpha()
+start_surf = pygame.transform.scale(start_surf, (800, 800))
+
+start_title = game_font.render("Snake", True, "Black")
+start_title_rect = start_title.get_rect(center = (400, 200))
+
+
 update_screen=pygame.USEREVENT
 pygame.time.set_timer(update_screen,150)
 
 main=Main()
 
 while True:
-    if game_state == True:
+    if not game_state:
+        if main.score != 0:
+            main.snake.undo()
+            main.snake.draw_end_snake()
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                
+                if event.type ==pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        game_state = True
+                        main = Main()
+        else:
+            game_screen.blit(start_surf, (0,0))
+            game_screen.blit(start_title, start_title_rect)
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                
+                if event.type ==pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        game_state = True
+                        main = Main()
+
+    else:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 pygame.quit()
@@ -239,22 +337,9 @@ while True:
                     
                 
         game_screen.blit(background, (0, 0))        
-        #game_screen.fill((20,120,150))   
-        main.draw_elements()
+        #game_screen.fill((20,120,150))
         check_game_over(main)
-
-    else:
-        game_screen.fill((20,120,150)) 
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            
-            if event.type ==pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    game_state = True
-                    main = Main()
-    
+        if game_state: main.draw_elements()  
     
     pygame.display.update()
     
